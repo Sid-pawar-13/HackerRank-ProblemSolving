9f0193690d8f6a0591129a20064bb8fcd406b786
Siddhant Pawar <siddhantpawar13.sp@gmail.com>
2020-11-19 21:19:38 +0530

    almost done

--- CountingValleys.py
+++ CountingValleys.py
@@ -1,4 +1,14 @@
+import random
 import numpy as np
 def countingValleys(steps, path):
+    print(steps,path)
+    alt = valley = 0
+    for step in path:
+        alt += 1 if step == 'U' else -1
+        if(step == 'U' and alt == 0):
+            valley += 1
+    return valley
 
-print(countingValleys())
+mylist = random.choices(["D","U"], k = int(np.random.randint(1,100,size=1)))
+print(countingValleys(len(mylist),
+                      mylist))
